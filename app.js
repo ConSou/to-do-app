b78db177d7edbd095a182939e9567d5dcc3d266b
Conor Souhrada <ConorSouhrada@gmail.com>
2018-03-06 12:51:26 -0700

    add app.js and update index.html

---
+++ app.js
@@ -0,0 +1,39 @@
+function onReady(){
+  const addToDoForm = document.getElementById("addToDoForm");
+  const newToDoText = document.getElementById("newToDoText");
+  const toDoList = document.getElementById("toDoList");
+
+  addToDoForm.addEventListener("submit", event => {
+    event.preventDefault();
+
+    //Get text
+    let title = newToDoText.value;
+
+    //Create list item with text
+    let newLi = document.createElement("li");
+
+    //Creates an input for list item
+    let checkbox = document.createElement("input");
+
+    //makes input checkbox type
+    checkbox.type = "checkbox";
+
+    //makes text title of list item
+    newLi.textContent = title;
+
+    //attaches checkbox to list item
+    newLi.appendChild(checkbox);
+
+    //Attaches li to todo toDoList
+    toDoList.appendChild(newLi);
+
+    //Creates empty input for next todo item
+    newToDoText.value = "";
+  });
+
+
+}
+
+window.onload = function(){
+  onReady();
+};
